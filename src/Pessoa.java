7e1047574391a562d26bf4ef0952e4fcb29ec9b7
ruannzin <ruanpablo24052006@gmail.com>
2023-05-18 08:28:13 -0300

    Eu entendi que declaramos um objeto, chamado "Pessoa", instanciamos o objeto,definimos o nome e sobrenome desse objeto,e acionamos para o objeto falar.

---
+++ src/Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa {
+     // Atributos da classe
+    String nome;
+    String sobrenome;
+    // Método da classe
+    public void falar(){
+        System.out.println("falei");
+    }
+
+
+}
